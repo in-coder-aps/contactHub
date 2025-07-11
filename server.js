e7b5810e8da6ed7ec52cf7f6afcf7b5bbc4cbb7f
Aditya Pratap Singh <singhaps2208@gmail.com>
2025-07-11 19:12:12 +0530

    feat: connect to MongoDB, define contact model, and complete CRUD controller logic

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const connectDb =require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler")
-const dorenv = require("dotenv").config();
+const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(express.json()); //In built Express parser
